d2481782feed5ace66e11ce5e8d45c2a2ede763a
Irori235 <Irori235@gmail.com>
2024-09-26 12:57:52 +0900

    wip

---
+++ binary-tree/src/binary_tree.rs
@@ -0,0 +1,136 @@
+// Referred to "Welcome to Comprehensive Rust - Exercise: Binary Tree"
+// https://google.github.io/comprehensive-rust/smart-pointers/exercise.html
+
+/// A node in the binary tree.
+#[derive(Debug)]
+struct Node<T: Ord> {
+    value: T,
+    left: Subtree<T>,
+    right: Subtree<T>,
+}
+
+/// A possibly-empty subtree.
+#[derive(Debug)]
+struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+
+/// A container storing a set of values, using a binary tree.
+///
+/// If the same value is added multiple times, it is only stored once.
+#[derive(Debug)]
+pub struct BinaryTree<T: Ord> {
+    root: Subtree<T>,
+}
+
+impl<T: Ord> BinaryTree<T> {
+    fn new() -> Self {
+        Self {
+            root: Subtree::new(),
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        self.root.insert(value);
+    }
+
+    fn has(&self, value: &T) -> bool {
+        self.root.has(value)
+    }
+
+    fn len(&self) -> usize {
+        self.root.len()
+    }
+}
+
+// Implement `new`, `insert`, `len`, and `has` for `Subtree`.
+impl<T: Ord> Subtree<T> {
+    fn new() -> Self {
+        Self(None)
+    }
+
+    fn insert(&mut self, value: T) {
+        match self.0 {
+            None => {
+                self.0 = Some(Box::new(Node {
+                    value,
+                    left: Subtree::new(),
+                    right: Subtree::new(),
+                }))
+            }
+            Some(ref mut node) => {
+                if value < node.value {
+                    node.left.insert(value);
+                } else if value > node.value {
+                    node.right.insert(value);
+                }
+            }
+        }
+    }
+
+    fn has(&self, value: &T) -> bool {
+        match self.0 {
+            None => false,
+            Some(ref node) => {
+                if *value == node.value {
+                    true
+                } else if *value < node.value {
+                    node.left.has(value)
+                } else {
+                    node.right.has(value)
+                }
+            }
+        }
+    }
+
+    fn len(&self) -> usize {
+        match self.0 {
+            None => 0,
+            Some(ref node) => 1 + node.left.len() + node.right.len(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let mut tree = BinaryTree::new();
+        assert_eq!(tree.len(), 0);
+        tree.insert(2);
+        assert_eq!(tree.len(), 1);
+        tree.insert(1);
+        assert_eq!(tree.len(), 2);
+        tree.insert(2); // not a unique item
+        assert_eq!(tree.len(), 2);
+    }
+
+    #[test]
+    fn has() {
+        let mut tree = BinaryTree::new();
+        fn check_has(tree: &BinaryTree<i32>, exp: &[bool]) {
+            let got: Vec<bool> = (0..exp.len()).map(|i| tree.has(&(i as i32))).collect();
+            assert_eq!(&got, exp);
+        }
+
+        check_has(&tree, &[false, false, false, false, false]);
+        tree.insert(0);
+        check_has(&tree, &[true, false, false, false, false]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(3);
+        check_has(&tree, &[true, false, false, true, true]);
+    }
+
+    #[test]
+    fn unbalanced() {
+        let mut tree = BinaryTree::new();
+        for i in 0..100 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.len(), 100);
+        assert!(tree.has(&50));
+    }
+}
